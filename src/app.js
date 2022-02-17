bea35f4ae577eb5c6f2452f406061bcdd9cc3048
John Steckelberg <jsteckelberg@nteglobal.com>
2022-02-17 13:48:24 -0600

    get port ready for heroku

--- src/app.js
+++ src/app.js
@@ -6,6 +6,8 @@ const { geocode } = require('./utils/geocode');
 
 const app = express();
 
+const port = process.env.PORT || 3000;
+
 // Define paths for Express
 const viewsPath = path.join(__dirname, '../templates/views');
 const publicPath = path.join(__dirname, '../public');
@@ -104,7 +106,7 @@ app.get('*', (req, res) => {
   });
 });
 
-app.listen(3000, () => {
+app.listen(port, () => {
   console.log('you\'ve been served');
 });
 
