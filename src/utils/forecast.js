14adf063451c9cc5c7042c557305c580ca98d49c
John Steckelberg <jsteckelberg@nteglobal.com>
2022-02-17 16:36:46 -0600

    update the weather report text

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -9,13 +9,18 @@ const forecast = (latitude, longitude, callback) => {
     } else if (body.error) {
       return callback(body.error, undefined);
     } else {
+      console.log(body.current);
       const {
         temperature,
         feelslike,
         weather_descriptions,
+        wind_speed,
+        wind_dir,
+        precip,
       } = body.current;
       const summary = weather_descriptions[0];
-      callback(undefined, `${summary} with a temp of ${temperature}, but feels like ${feelslike}`);
+      callback(undefined, `${summary} with a temp of ${temperature}, but feels like ${feelslike}.  
+      The wind speed is ${wind_speed} to the ${wind_dir}.  There is a ${precip * 100}% chance of precipitation.`);
     }
   });
 };
